f55dba0e0f0ac85760ebc58ccff012c4c54b04c4
dotSality <90727519+dotSality@users.noreply.github.com>
2021-11-27 18:54:17 +0300

    Publish
    Publishing counter project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
+import {restoreState, saveState} from './LocalStorageFunctions/storage';
+
+export type ValuesType = {
+    [key: string]: number
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initialState: ValuesType = {
+        max: 0,
+        start: 0
+    }
+
+    const [values, setValues] = useState<ValuesType>(initialState)
+    const [count, setCount] = useState<number>(0)
+    const [countMsg, setCountMsg] = useState<string>('')
+    const [error, setError] = useState<boolean>(false)
+    const [max, setMax] = useState<number>(0)
+    const [start, setStart] = useState<number>(0)
+
+    useEffect(() => {
+        let state = restoreState<ValuesType>('values', values)
+        if (state) {
+            setError(false)
+            setValues(state)
+            setMax(state.max)
+            setStart(state.start)
+            setCount(state.start)
+        } else {
+            setError(true)
+            setCountMsg('enter values and press "set"')
+        }
+    }, [])
+
+    const onMaxInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        restoreState<ValuesType>('values', values) ? setError(false) : setError(true)
+        let value = parseInt(e.currentTarget.value)
+        setMax(value)
+        setCountMsg('enter values and press "set"')
+        if (value < 0) {
+            setError(true)
+            setCountMsg('value incorrect')
+        } else if (value <= start) {
+            setError(true)
+            setCountMsg('value incorrect')
+        } else setError(false)
+    }
+
+    const onStartInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        restoreState<ValuesType>('values', values) ?  setError(true) : setError(false)
+        let value = parseInt(e.currentTarget.value)
+        setStart(value)
+        setCountMsg('enter values and press "set"')
+        if (value < 0) {
+            setError(true)
+            setCountMsg('value incorrect')
+        } else if (value >= max) {
+            setError(true)
+            setCountMsg('value incorrect')
+        } else setError(false)
+    }
+
+    const setCurrentStorage = () => {
+        let newValues = {...values, max: max, start: start}
+        setValues(newValues)
+        setCount(newValues.start)
+        saveState<ValuesType>('values', newValues)
+    }
+
+    const setCurrentCount = () => setCount(count + 1)
+    const resetCurrentCount = () => {
+        let state = restoreState<ValuesType>('values', values)
+        if (state) setCount(state.start)
+    }
+
+    const output = restoreState<ValuesType>('values', values) ? (error ? countMsg : count) : countMsg
+
+    const errorClassName = error ? 'error' : ''
+    return (
+        <div className={'head-container'}>
+            <div className={'container'}>
+                <label>Set max: <input className={errorClassName} onChange={onMaxInputChange} value={max} type='number'/></label>
+                <label>Set min: <input className={errorClassName} onChange={onStartInputChange} value={start} type='number'/></label>
+                <div className={'button-container'}>
+                    <button
+                        disabled={error}
+                        className={'button'}
+                        onClick={setCurrentStorage}>set
+                    </button>
+                </div>
+            </div>
+            <div className={'container'}>
+                <span className={errorClassName}>{output}</span>
+                <div className={'button-container'}>
+                    <button
+                        disabled={count === values.max ? true : error}
+                        className={'button'}
+                        onClick={setCurrentCount}>count
+                    </button>
+                    <button
+                        disabled={count === values.start ? true : error}
+                        className={'button'}
+                        onClick={resetCurrentCount}>reset
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/LocalStorageFunctions/storage.ts
@@ -0,0 +1,12 @@
+export function saveState<T> (key: string, state: T) {
+    localStorage.setItem(key, JSON.stringify(state))
+}
+
+export function restoreState<T>(key: string, defaultState: T) {
+    let state = defaultState
+    const stateAsString = localStorage.getItem('values')
+    if (stateAsString) {
+        state = JSON.parse(stateAsString)
+        return state
+    } else return null
+}
